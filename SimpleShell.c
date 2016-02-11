7aedc0fd5a297b0d177ab67e9187cfc6477da1ff
Jacqueline Catlin <your.email@strath.ac.uk>
2016-02-11 12:08:32 +0000

    Author Addition
    Cameron Turner

--- SimpleShell.c
+++ SimpleShell.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 @file         SimgpleShell.c
-@author       Darren Lowe, Jacqueline Catlin, Georgia Greegoriou,
+@author       Darren Lowe, Jacqueline Catlin, Georgia Greegoriou, Cameron Turner
 @date         Thursday, 11/02/2016
 @brief
 *******************************************************************************/
