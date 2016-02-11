3843512a4c8d6e1f4f7c3988e592bdef4aad3934
F1r3Arr0w <georgia4university@gmail.com>
2016-02-11 11:58:24 +0000

    Added Author

--- SimpleShell.c
+++ SimpleShell.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 @file         SimgpleShell.c
-@author       Darren Lowe, Jacqueline Catlin, ,
+@author       Darren Lowe, Jacqueline Catlin, Georgia Greegoriou,
 @date         Thursday, 11/02/2016
 @brief
 *******************************************************************************/
