efafc62eef6d93826520c22cd9c9520f35967567
Jacqueline Catlin <your.email@strath.ac.uk>
2016-02-11 12:08:59 +0000

    Author Addition
    Ethan Dickson

--- SimpleShell.c
+++ SimpleShell.c
@@ -1,6 +1,6 @@
 /******************************************************************************
 @file         SimgpleShell.c
-@author       Darren Lowe, Jacqueline Catlin, Georgia Greegoriou, Cameron Turner
+@author       Darren Lowe, Jacqueline Catlin, Georgia Greegoriou, Cameron Turner, Ethan Dickson
 @date         Thursday, 11/02/2016
 @brief
 *******************************************************************************/
