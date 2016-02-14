ff2e93e88e387ce77117f0868e8578c3cbb2a261
EthanDMan <e.dickson909@gmail.com>
2016-02-14 16:57:08 +0000

    refactoring

--- SimpleShell.c
+++ SimpleShell.c
@@ -15,24 +15,15 @@ int main() {
 
     char line [MAXIN];
     char delim[] = " ";
-
-    //strings for comparison
-    char str1[15];
-    char str2[15];
-    //newline char as enter is typed
-    strcpy(str1, "exit\n");
-    strcpy(str2, "<ctrl>-D\n");
-
-    int i = 1;
     const char* token;
-    while (i == 1) {
+    while (1) {
         do {
             printf(">");
             fgets(line, MAXIN, stdin);
         }  while (strlen(line) <= 1);
         token = strtok(line, delim);
         while (token != NULL){
-            if ((strcmp(str1, token) == 0) || (strcmp(str2, token) == 0))  {
+            if ((strcmp("exit\n", token) == 0) || (strcmp("<ctrl>-D\n", token) == 0))  {
                     printf("Quitting\n");
                     return 1;
             }
