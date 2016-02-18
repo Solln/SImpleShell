ef788340798438cb1c129faac2aa95f2ebc91a0b
EthanDMan <e.dickson909@gmail.com>
2016-02-18 12:52:56 +0000

    Tokens[]

--- SimpleShell.c
+++ SimpleShell.c
@@ -9,47 +9,51 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
+//for fork()
+#include <unistd.h>
+#include <errno.h>
+#include <sys/types.h>
 
 
 #define MAXIN 512
+#define MAXPARA 50
 #define INERR -1
 
+char filePath[] = "C:\\";
+
+int setenv() {
+    printf("Current filepath is %s\n",filePath);
+}
 
 int main() {
 char line [MAXIN];
 char delim[] = " \t|><,&;";
-char* token;
+char *token;
+char *tokens[MAXPARA];
 bool exitShell = false;
 
+pid_t pid;
+int *ret_status;
 
-
-    while (exitShell == false) {
-
-        do{
-            printf(">");
-            fgets(line, MAXIN, stdin);
-        } while (strlen(line) <= 1);
-        (line[strlen(line)-1] == '\n')? line[strlen(line)-1] = '\0' : 0;
-        token = strtok(line, delim);
-        if ((strcmp("exit", token) == 0) || (strcmp(EOF, token) == 0))  {
-                    printf("Quitting\n");
-                    exitShell = true;
-                    //return 1;
-            }
-        while (token != NULL){
-         /*   if ((strcmp("exit", token) == 0) || (strcmp("^D", token) == 0))  {
-                    printf("Quitting\n");
-                    exitShell = true;
-                    //return 1;
-            }*/
-            printf("token = (%s)\n", token);
-            token = strtok(NULL, delim);
-        }
+while (exitShell == false) {
+    do{
+        printf("%s>", filePath);
+        fgets(line, MAXIN, stdin);
+    } while (strlen(line) <= 1);
+    (line[strlen(line)-1] == '\n')? line[strlen(line)-1] = '\0' : 0;
+    token = strtok(line, delim);
+    int i = 0;
+    for (token = strtok(line, delim); tokens[i++] = token; token = strtok(NULL, delim));
+    i = 0;
+    for(i = 0; i < 3; i++)
+        printf("token = (%s)\n", tokens[i]);
+    if ((strcmp("exit", tokens[0]) == 0))  {
+        printf("Quitting\n");
+        exitShell = true;
+    }
     }
     return 0;
 }
-
 
 /* Moving onto task two, checked about adding the fork and wait statement. but It broke the exit command.
  Entered this code in where its marked ***Here***
