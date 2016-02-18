42dca0cff37cd4ce837de7d91a398b3fcdd82e5c
Darren Lowe <yxb15183@strath.ac.uk>
2016-02-18 15:25:19 +0000

    Full Shell Refactor
    Changed the code to be more readable, also added the execute function
    and the fork process. This version does not use tokens but splits the
    string up with strep

--- SimpleShell.c
+++ SimpleShell.c
@@ -9,77 +9,96 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
 
 
-
+void parseLine(char* cmd, char** params);
+int executeLine(char** params);
+ 
 #define MAXIN 512
-#define INERR -1
+#define MAXPARA 50
 
-
-int main() {
-char line [MAXIN];
-char delim[] = " \t|><,&;";
-char* token;
-bool exitShell = false;
-
-
-
-    while (exitShell == false) {
-
-        do{
-            printf(">");
-            fgets(line, MAXIN, stdin);
-        } while (strlen(line) <= 1);
-        (line[strlen(line)-1] == '\n')? line[strlen(line)-1] = '\0' : 0;
-        token = strtok(line, delim);
-        if ((strcmp("exit", token) == 0) || (strcmp(EOF, token) == 0))  {
-                    printf("Quitting\n");
-                    exitShell = true;
-                    //return 1;
-            }
-        while (token != NULL){
-         /*   if ((strcmp("exit", token) == 0) || (strcmp("^D", token) == 0))  {
-                    printf("Quitting\n");
-                    exitShell = true;
-                    //return 1;
-            }*/
-            printf("token = (%s)\n", token);
-            token = strtok(NULL, delim);
+int main()
+{
+    char line[MAXIN];
+    char* strings[MAXPARA];
+    
+    
+    while(1){
+        
+        printf(">");
+        
+        if (fgets(line, sizeof(line), stdin) == NULL) break;
+        
+        if (line[strlen(line)-1] == '\n') {
+            line[strlen(line)-1] = '\0';
         }
+        
+        
+        parseLine(line, strings);
+    
+        
+        if (strcmp(strings[0], "exit") == 0) break;
+        
+        
+        if (executeLine(strings) == 0) break;
+        
     }
+    
     return 0;
 }
+    
 
-
-/* Moving onto task two, checked about adding the fork and wait statement. but It broke the exit command.
- Entered this code in where its marked ***Here***
-
-
- else
-
+void parseLine(char* line, char** strings)
 {
-    pid = fork();
-    if (pid){
-        printf("Waiting for child (%d)\n", pid);
-        pid = wait(ret_status);
-        printf("Child (%d) finished\n", pid);
-    }
+    
+    for(int i = 0; i < MAXPARA; i++) {
+        strings[i] = strsep(&line, " ");
+        if(strings[i] == NULL) break;
+    
+}
 }
 
-Had these variables set as well at the top:
-
- pid_t pid;
- int *ret_status;
-
-As well as these includes:
- #include <unistd.h>
- #include <errno.h>
- #include <sys/types.h>
-
-Unsure as to exact implementation, was using http://rik0.altervista.org/snippets/csimpleshell.html as advice.
 
 
 
+    int executeLine(char** strings)
+    {
+        // Fork process
+        pid_t pid = fork();
+        
+        // Error
+        if (pid < 0) {
+            char* error = strerror(errno);
+            printf("fork: %s\n", error);
+            return 1;
+        }
+        
+        // Child process
+        else if (pid == 0) {
+            // Execute command
+            execvp(strings[0], strings);
+            
+            // Error occurred
+            char* error = strerror(errno);
+            printf("shell: %s: %s\n", strings[0], error);
+            return 0;
+        }
+        
+        // Parent process
+        else {
+            // Wait for child process to finish
+            int childStatus;
+            waitpid(pid, &childStatus, 0);
+            return 1;
+        }
+        
+    }
+
+
+
+/*
 
    Need to error trap for White space input (can trap other error input here as well)
    Need to add the exit command to break the loop
