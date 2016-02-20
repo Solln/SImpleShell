4ef21b3637da9b34b86b8e325c2ea6ad07e08d36
EthanDMan <e.dickson909@gmail.com>
2016-02-20 01:43:03 +0000

    Edited to use tokens
    ctrl-D and EOF comparison still not implemented

--- SimpleShell.c
+++ SimpleShell.c
@@ -16,78 +16,83 @@
 
 void parseLine(char* cmd, char** params);
 int executeLine(char** params);
- 
+
 #define MAXIN 512
 #define MAXPARA 50
 
 int main()
 {
     char line[MAXIN];
+    char delim[] = " |><,&;";
+    char* token;
     char* strings[MAXPARA];
-    
+
+    memset(strings, 0, sizeof(strings));
+
     char filePath[] = "C:\\";
 
-    
-    while(1){
-        
+    bool exitShell = false;
+    while(!exitShell){
+
         printf("%s>", filePath);
-        
+
         if (fgets(line, sizeof(line), stdin) == NULL) break;
-        
-        if (line[strlen(line)-1] == '\n') {
-            line[strlen(line)-1] = '\0';
+
+        (line[strlen(line)-1] == '\n')? line[strlen(line)-1] = '\0' : 0;
+
+        token = strtok(line, delim);
+        int i = 0;
+        while (token != NULL){
+            strings[i] = token;
+            if ((strcmp("exit", strings[0]) == 0))  {
+                    printf("Quitting\n");
+                    exitShell = true;
+                    break;
+                    //return 1;
+            }
+            printf("token = %s\n", strings[i++]);
+            token = strtok(NULL, delim);
         }
-        
-        
-        parseLine(line, strings);
-    
-        
-        if (strcmp(strings[0], "exit") == 0) break;
-        
-        
-        if (executeLine(strings) == 0) break;
-        
+
+        executeLine(strings) == 0) break;
+
+        //To flush strings at the end of each cycle of input
+        memset(strings, 0, sizeof(strings));
     }
 }
 
-        
-        void parseLine(char* line, char** strings)
-
-        {
-    
-            for(int i = 0; i < MAXPARA; i++) {
-                strings[i] = strsep(&line, " ");
-                if(strings[i] == NULL) break;
-    
-                }
+/*
+void parseLine(char* line, char** strings) {
+        for (int i = 0; i < MAXPARA; i++) {
+            strings[i] = strsep(&line, " ");
+            if(strings[i] == NULL) break;
         }
+}
+*/
 
-
-
-
-    int executeLine(char** strings)
-    {
+int executeLine(char** strings)
+{
         // Fork process
         pid_t pid = fork();
-        
+
         // Error
         if (pid < 0) {
             char* error = strerror(errno);
             printf("fork: %s\n", error);
             return 1;
         }
-        
+
         // Child process
         else if (pid == 0) {
             // Execute command
             execvp(strings[0], strings);
-            
+
             // Error occurred
             char* error = strerror(errno);
             printf("shell: %s: %s\n", strings[0], error);
             return 0;
         }
-        
+
         // Parent process
         else {
             // Wait for child process to finish
@@ -95,10 +100,8 @@ int main()
             waitpid(pid, &childStatus, 0);
             return 1;
         }
-        
+
     }
-
-
 
 /*
 
