21222ea586edc652236010acc67e389cf01798da
Darren Lowe <yxb15183@strath.ac.uk>
2016-03-17 11:41:16 +0000

    Added main bulk of Stage 5

--- SimpleShell.c
+++ SimpleShell.c
@@ -23,9 +23,13 @@ int setPath( char*);
 #define MAXIN 512
 #define MAXPARA 50
 #define MAXPATH 120
+#define HISTORY_COUNT 20
 
 const char* currentDir;
 char* path;
+
+int history(char *hist[], int current);
+int clear_history(char *hist[]);
 
 char* getPath() { 
     printf("%s\n", getenv("PATH"));
@@ -78,7 +82,15 @@ int main()
     memset(strings, 0, sizeof(strings));
     char filePath[MAXPATH];
     chdir(getenv("HOME"));
+    char *hist[HISTORY_COUNT];
+    
+    
+    int i, current = 0;
+    
+    for (i = 0; i < HISTORY_COUNT; i++)
+        hist[i] = NULL;
 
+    
     bool exitShell = false;
     while(!exitShell) {
 
@@ -95,6 +107,14 @@ int main()
             //printf("token = %s\n", strings[i++]);
             token = strtok(NULL, delim);
         }
+        
+        free(hist[current]);
+        
+        hist[current] = strdup(line);
+        
+        current = (current + 1) % HISTORY_COUNT;
+
+        
 	if (strings[0] == NULL) continue;
         if ((strcmp("exit", strings[0]) == 0))  {                    
                     exitShell = true;
@@ -106,6 +126,12 @@ int main()
         else if (strcmp("setpath", strings[0]) == 0) {
             if (setPath(strings[1])) perror("setenv error:");
         }
+        
+        else if (strcmp(line, "history") == 0)
+            history(hist, current);
+        
+        else if (strcmp(line, "hc") == 0)
+            clear_history(hist);
 	       
         //Code for the cd command, needs to change the directory name when showing the path.
          
@@ -128,6 +154,37 @@ int main()
    getPath();
    printf("Quitting\n");
    return 0;
+}
+
+
+int history(char *hist[], int current)
+{
+    int i = current;
+    int hist_num = 0;
+    
+    do {
+        if (hist[i]) {
+            printf("%4d  %s\n", hist_num, hist[i]);
+            hist_num++;
+        }
+        
+        i = (i + 1) % HISTORY_COUNT;
+        
+    } while (i != current);
+    
+    return 0;
+}
+
+int clear_history(char *hist[])
+{
+    int i;
+    
+    for (i = 0; i < HISTORY_COUNT; i++) {
+        free(hist[i]);
+        hist[i] = NULL;
+    }
+    
+    return 0;
 }
 
 /*
