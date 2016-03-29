7115b3660a9540d432076d1ef4129f3c48e87b7d
EthanDMan <e.dickson909@gmail.com>
2016-03-29 08:51:32 +0100

    Added files via upload
    Halfway through stage 7

--- SimpleShell.c
+++ SimpleShell.c
@@ -14,33 +14,95 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
 #define MAXIN 512
 #define MAXPARA 50
 #define MAXPATH 120
 #define HISTORY_COUNT 20
+#define ALLIAS_COUNT 10
 
 #define NoPError "function takes no parameters"
 #define UnExPError "unexpected parameters"
 #define PathError "path expected"
+#define InValError "invalid input"
+#define RangeError "command out of range"
 
-const char* currentDir;
-char* path;
-int current = 0;
-char *hist[HISTORY_COUNT];
+typedef struct {
+    int number;
+    char command[MAXIN];
+} history_t;
 
+typedef struct {
+    char name[MAXIN];
+    char command[MAXIN];
+} allias_t;
 
-char* getPath(char** strings) {
+void invokeHistory(history_t * hist[], char* line, int current) {
+    char *input = strdup(line);
+    int number;
+    if (strcspn(input, "!") == 0) {
+        if (sscanf(++input, "%d", &number) == 1) {
+            if (number < 0) {
+                if (number > (0-HISTORY_COUNT) && number >= (0-current)) {
+                    strncpy(line, hist[(current+number)%HISTORY_COUNT]->command, MAXIN);
+                }
+                else printf("error: %s\n", RangeError);
+            }
+            else if (number > (current-HISTORY_COUNT) && number <= current) {
+                strncpy(line, hist[(number-1)%HISTORY_COUNT]->command, MAXIN);
+            }
+            else printf("error: %s\n", RangeError);
+        }
+        else printf("error: %s\n", InValError);
+    }
+}
+
+void getLine(char *line) {
+    if (fgets(line, MAXIN, stdin) != NULL)
+        (line[strlen(line)-1] == '\n')? line[strlen(line)-1] = '\0' : 0;
+    else {
+        strcpy(line, "exit");
+    }
+}
+
+void splitLine(char *line, char* strings[]) {
+    char delim[] = " |><,&;\t";
+    char* token;
+    token = strtok(line, delim);
+    int i = 0;
+    while (token != NULL) {
+        strings[i++] = token;
+        token = strtok(NULL, delim);
+    }
+}
+
+int execute(char* strings[]) {
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror(strings[0]);
+        return 1;
+    }
+    else if (pid == 0) {
+        execvp(strings[0], strings);
+        perror(strings[0]);
+        exit(0);
+    }
+    else {
+        wait(NULL);
+        return 1;
+    }
+}
+
+char* getPath(char* strings[]) {
+    char* path;
     if (strings[1] != NULL) {
 	printf("getpath error: %s\n", NoPError);
 	return "";
     }
-    printf("%s\n", getenv("PATH"));
+    printf("%s\n",(path = getenv("PATH")));
     return path;
 }
 
-int setPath(char** strings) {
-    int returnVal = 0;
+int setPath(char* strings[]) {
     if (strings[2] != NULL) {
 	printf("setpath error: %s\n", UnExPError);
     }
@@ -49,286 +111,184 @@ int setPath(char** strings) {
     }
     else {
 	char* newPath = strings[1];
-    	returnVal = setenv("PATH", newPath, 1);
+    	if (setenv("PATH", newPath, 1) == 0) return 1;
+    	else perror("setpath error");
     }
-    return returnVal;
+    return 0;
 }
 
-int changeDirectory(char** strings) {
-   int returnVal = 1;
+void changeDirectory(char* strings[]) {
    if (strings[2] != NULL) {
         printf("cd error: %s\n", UnExPError);
    }
    else if (strings[1] == NULL) {
         chdir(getenv("HOME"));
-        returnVal = 0;
-   } else if (chdir(strings[1])) perror("cd");
-   return returnVal;
+   } else if (chdir(strings[1]) != 0) perror("cd");
 }
 
-int execute(char** strings) {
-    // Fork process
-    pid_t pid = fork();
-    // Error
-    if (pid < 0) {
-        perror(strings[0]);
-        return 1;
-    }
-    // Child process
-    else if (pid == 0) {
-        // Execute command
-        execvp(strings[0], strings);
-        // Error occurred
-        perror(strings[0]);
-        exit(0);
-    }
-    // Parent process
+void printHistory(history_t *hist[], char* strings[]) {
+    if (strings[1] != NULL) printf("history error: %s\n", UnExPError);
     else {
-        // Wait for child process to finish
-        wait(NULL);
-        return 1;
+        int current = 0;
+        int i = 0;
+        while (hist[i] != NULL && i < HISTORY_COUNT) {
+            if (current < (hist[i]->number)) {
+                current = hist[i]->number;
+            }
+            i++;
+        }
+        i = (current % HISTORY_COUNT);
+        do {
+            if (hist[i] != NULL) {
+                printf("%d: %s\n", hist[i]->number, hist[i]->command);
+            }
+            i = ((i + 1) % HISTORY_COUNT);
+        } while (i != (current % HISTORY_COUNT));
     }
 }
 
-int printHistory()
-{
-    int i = ((current)  % HISTORY_COUNT);
-    int order = 1;
-    do {
-        if (strcmp("", hist[i]) != 0) {
-            printf("%4d  %s\n", order, hist[i]);
-            order++;
+void addAllias(allias_t* allias[], char* strings[]) {
+    if (strings[3] != NULL) printf("allias error: %s", UnExPError);
+    for (int i = 0; i < ALLIAS_COUNT; i++) {
+        if (allias[i] != NULL) {
+            if (strcmp(strings[1], allias[i]->name) == 0) {
+                printf("overwriting allias %s\n", strings[1]);
+                strncpy(allias[i]->command, strings[2], MAXIN);
+                return;
+            }
+            else if (i == 9) printf("Alliases full!\n");
         }
-        i = ((i + 1) % HISTORY_COUNT);
-    } while (i != (current % HISTORY_COUNT) && hist[i] != NULL);
-    return 0;
-}
-
-int clear_history()
-{
-    for (int i = 0; i < HISTORY_COUNT; i++)
-        hist[i] = malloc(MAXIN);
-    return 0;
-}
-
-char* getHistory(char* line) {
-    int number;
-    ++line;
-    if (line[0] == '-') {
-        if (sscanf(++line, "%d", &number) == 1) {
-            if (number > 0 && number < 20 && number < current)
-                return hist[(current % HISTORY_COUNT) - (number)];
+        else {
+            strncpy(allias[i]->name, strings[1], MAXIN);
+            strncpy(allias[i]->command, strings[2], MAXIN);
         }
     }
-    else if (sscanf(line, "%d", &number) == 1) {
-        if (number > 0 && number < 20 && number < current)
-        return hist[number-1];
-    }
-    printf("Invalid input\n");
-    return "";
 }
 
-void executeLine(char** strings) {
-    if (strcmp("getpath", strings[0]) == 0) {
+void unAllias(allias_t* allias[], char* strings[]) {
+    if (strings[2] != NULL) printf("unallias error: %s\n", UnExPError);
+    else for (int i = 0; i < ALLIAS_COUNT; i++) {
+        if (allias[i] != NULL) {
+            if (strcmp(strings[1], allias[i]->command) == 0) {
+                allias[i] == NULL;
+            }
+        }
+    }
+}
+
+void processTokens(allias_t* allias[], history_t* hist[], char* strings[], bool* exitShell) {
+    if ((strcmp("exit", strings[0]) == 0))  {
+            if (strings[1] != NULL) printf("exit error: %s\n", NoPError);
+            else *exitShell = true;
+    }
+    else if (strcmp("getpath", strings[0]) == 0) {
         getPath(strings);
     }
     else if (strcmp("setpath", strings[0]) == 0) {
         setPath(strings);
     }
-    else if (strcmp("history", strings[0]) == 0)
-        printHistory();
-    else if (strcmp("hc", strings[0]) == 0)
-        clear_history();
     else if (strcmp("cd", strings[0]) == 0){
         changeDirectory(strings);
     }
+    else if (strcmp("history", strings[0]) == 0)
+        printHistory(hist, strings);
+    else if (strcmp("allias", strings[0]) == 0) {
+        allias(allias, strings);
+    }
+    else if (strcmpe("unallias", strings[0]) == 0) {
+        unAllias(allias, strings);
+    }
     else execute(strings);
 }
-void writeHistory(){
-    
-    FILE *fp;
-    
-    //char str[80];
-    //strcpy(str, getenv("HOME"));
-    //strcat(str, "hist_list.txt");
-    
-    int n;
-    
-    fp=fopen("hist_list.txt","w");
-    
-    for(n=0;n<HISTORY_COUNT;n++) {
-        
-        fprintf(fp,"%s\n",hist[n]);
-        
-    }
-    
-    fclose(fp);
-    
+
+void addHistory(history_t* hist[], int* current, char* line) {
+    history_t *newCommand = malloc(sizeof(history_t));
+    newCommand->number = *current + 1;
+    strncpy(newCommand->command, line, MAXIN);
+    hist[*current % HISTORY_COUNT] = newCommand;
+    *current = *current + 1;
 }
 
-void loadHistory(){
-    
-    //char str[80];
-    //strcpy(str, getenv("HOME"));
-    //strcat(str, "hist_list.txt");
-    
-    
-    if( access( "hist_list.txt", F_OK ) != -1 ) {
-        // file exists
-        int i = 0;
-        
-        FILE *fp = fopen("hist_list.txt", "r");
-        
-        if (fp == 0)
-        {
-            fprintf(stderr, "failed to open hist_list.txt\n");
-            exit(1);
+void checkAllias(allias_t* allias[], char* strings[]) {
+    for (int i = 0; i < ALLIAS_COUNT; i++) {
+        if (allias[i] != NULL) {
+            for (int j = 0; j < MAXPARA) {
+                if (strings[j] != NULL) {
+                    if (strcmp(allias[i]->name, strings[j]) == 0) {
+                        strncpy(strings[j], allias[i]->command, MAXIN);
+                    }
+                }
+            }
         }
-        while (i < HISTORY_COUNT && fgets(hist[i], sizeof(hist[0]), fp))
-        {
-            hist[i][strlen(hist[i])-1] = '\0';
-            i = i + 1;
+    }
+}
+
+void runShell(history_t* hist[], int* current) {
+    char *currentDir = malloc(sizeof(char)*MAXPATH);
+    char *line = malloc(sizeof(char)*MAXIN);
+    char *strings[MAXPARA];
+    memset(strings, 0, sizeof(strings));
+    allias_t *allias[ALLIAS_COUNT];
+    memset(allias, 0, sizeof(allias));
+    bool exitShell = false;
+    while(!exitShell) {
+        printf("%s>", getcwd( currentDir, MAXPATH));
+        getLine(line);
+        if (strcmp("", line) == 0) continue;
+        invokeHistory(hist, line, *current);
+        addHistory(hist, current, line);
+        splitLine(line, strings);
+        checkAllias(allias, strings);
+        processTokens(hist, strings, &exitShell);
+        memset(strings, 0, sizeof(strings));
+    }
+    free(line);
+}
+
+int loadHistory(history_t* hist[], int* current){
+    if( access( "hist_list.txt", F_OK ) != -1 ) {
+        char* line = malloc(sizeof(int)+(sizeof(char)*MAXIN));
+        FILE *fp = fopen("hist_list.txt", "r");
+        if (fp == 0) {
+            fprintf(stderr, "failed to open hist_list.txt\n");
+            perror("load");
+            return 0;
+        }
+        while (fgets(line, MAXIN, fp)) {
+            char* strings[2];
+            char* tempChar = malloc(sizeof(char)*MAXIN);
+            splitLine(line, strings);
+            sscanf(strings[0], "%d", current);
+            sscanf(strings[1], "%s", tempChar);
+            addHistory(hist, current, tempChar);
+            free(tempChar);
         }
         fclose(fp);
-        
+        free(line);
+        return 1;
     } else {
-        // file doesn't exist
-        
-        FILE *fp = fopen("hist_list.txt", "w");
-        
-    }   
+        fopen("hist_list.txt", "w");
+        return 0;
+    }
 }
 
-alias() {
-	if (1) {
-		for(int r=0; r < 10; r++) {
-			if (strcmp(strings[0], alias[r][0])) {
-				//execute
-				//code to execute alias command
-				break;
-			}
-		}
-	}
-	else if (strcmp("alias", strings[0]) == 0) && sizeof(strings) == 1) {
-		if (alias[0][0] == null)
-			printf("No Aliases");
-		else
-			for(int r=0; r < 10; r++) {
-				if (alias[r][0] != null) {
-					for(int c=0; c < 2; c++)
-						printf("%s", alias[r][c]);
-					printf("\n");
-				}
-			}
-	}
-	else if (strcmp("alias", strings[0]) == 0) && sizeof(strings) == 3) {
-		int placed = 0;
-		for (int r=0; r < 10; r++) {
-			if (strcmp(strings[2], alias[r][1]) == 0) {
-				printf("Alias %s Has Been Replaced With %s For Command %s", alias[r][0], strings[1], alias[r][2]);
-				alias[r][0] = strings[1];
-				placed = 1;
-				break;
-			}
-		}
-		for (int r=0; r < 10; r++) {
-			if (strcmp(strings[1], alias[r][0]) == 0) {
-				printf("Command %s Has Been Replaced With %s For Alias %s", alias[r][1], strings[2], alias[r][1]);
-				alias[r][1] = strings[2];
-				placed = 1;
-				break;
-			}
-		}
-		if (sizeof(alias) == 10) {
-			printf("Alias List Is Full");
-			placed = 1;
-		}
-		if (placed == 0) {
-			alias[sizeof(alias)][0] = strings[1];
-			alias[sizeof(alias)][1] = strings[2];
-		}
-	}
-	else if (strcmp("unalias", strings[0]) == 0) && sizeof(strings) == 2) {
-		int removed = 0;
-		for(int r=0; r < 10; r++) {
-			if (strcmp(strings[1], alias[r][0])) {
-				alias[r][0] = null;
-				alias[r][1] = null;
-				while(!(r == 9)) {
-					alias[r][0] = alias[r + 1][0];
-					alias[r][1] = alias[r + 1][0];
-					r = r + 1;
-				}
-				removed = 1;
-				break;
-			}
-		}
-		if (removed == 0)
-			printf("No Such Alias");
-	}
-	else if (strcmp("alias", strings[0]) == 0) && sizeof(strings) > 3) {
-		printf("Invalid Input");
-	}
+void writeHistory(history_t* hist[]){
+    FILE *fp;
+    fp=fopen("hist_list.txt","w");
+    for(int n=0;n<HISTORY_COUNT;n++) {
+        if (hist[n] != NULL) fprintf(fp,"%d: %s\n",hist[n]->number, strcat(hist[n]->command, " "));
+    }
+    fclose(fp);
 }
 
-
-int main()
-{
-    path = getenv("PATH");
-    char line[MAXIN];
-    char delim[] = " |><,&;\t";
-    char* token;
-    char* strings[MAXPARA];
-	char* alias[10][2];
-    memset(strings, 0, sizeof(strings));
-    char filePath[MAXPATH];
+int main() {
+    char* path = getenv("PATH");
     chdir(getenv("HOME"));
-    int i;
-    for (i = 0; i < HISTORY_COUNT; i++)
-        hist[i] = malloc(MAXIN);
-    loadHistory();
-    i = 0;
-    current = 0;
-    bool exitShell = false;
-	//alias() you need to check for an alias first
-    while(!exitShell) {
-        currentDir = (getcwd( filePath, MAXPATH ) != NULL)? filePath : "ERROR";
-        printf("%s>", currentDir);
-        if (fgets(line, sizeof(line), stdin) == NULL) break;
-        (line[strlen(line)-1] == '\n')? line[strlen(line)-1] = '\0' : 0;
-        if (line[0] == '!'){
-            strncpy(line, getHistory(line), MAXIN);
-        }
-        else if (strcmp("", line) == 0) continue;
-        strncpy(hist[current % HISTORY_COUNT], strdup(line), MAXIN);
-        ++current;
-        token = strtok(line, delim);
-        i = 0;
-        while (token != NULL){
-            strings[i++] = token;
-            token = strtok(NULL, delim);
-        }
-        if ((strcmp("exit", strings[0]) == 0))  {
-            if (strings[1] != NULL) printf("exit error: %s\n", NoPError);
-            else exitShell = true;
-        }
-        executeLine(strings);
-        memset(strings, 0, sizeof(strings));
-   }
-   memset(strings, 0, sizeof(strings));
-   strings[1] = path;
-   setPath(strings);
-   strings[1] = NULL;
-   getPath(strings);
-   writeHistory();
-   clear_history();
-   printf("Quitting\n");
-   return 0;
+    history_t *hist[HISTORY_COUNT];
+    memset(hist, 0, sizeof(hist));
+    int current = 0;
+    loadHistory(hist, &current);
+    runShell(hist, &current);
+    setenv("PATH", path, 1);
+    writeHistory(hist);
 }
-
-/*
-
-   Need to error trap for White space input (can trap other error input here as well)
-   Need to add the exit command to break the loop
-    Added 14/02/2016
-*/
